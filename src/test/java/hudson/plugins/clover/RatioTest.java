6f3a875ec1121fb12ed429f92bbaf4bdeae67b51
Brian Wightman <MidLifeXis@wightmanfam.org>
2013-09-11 12:43:12 -0500

    Comment out tests that I don't have a defined behavior
    These tests do not have a defined behavior, other than code that has been added over time without concern with invalid data.

--- src/test/java/hudson/plugins/clover/RatioTest.java
+++ src/test/java/hudson/plugins/clover/RatioTest.java
@@ -29,13 +29,23 @@ public class RatioTest extends TestCase {
      * 1 - Ratios where the denominator is 0 (no tests), return 100%.
      * 
      * 2 - All other ratios return the numerator / denominiator.
+     * 
+     * There is a case here that is in the code, but is caused by invalid data:
+     * 
+     * 3 - The numerator is larger than the denominator.
+     * 
+     * There are other invalid data points as well:
+     * 
+     * 4 - Numerator is larger than the denominator
+     * 
+     * 5 - Either the numerator or the denominator is < 0
      *
      * @throws Exception
      */
     public void testGetPercentageFloat() throws Exception {
         assertEquals("0/0   => 100", 100.0f, Ratio.create(  0,0).getPercentageFloat(), 0.005f);
-        assertEquals("1/0   => 100", 100.0f, Ratio.create(  1,0).getPercentageFloat(), 0.005f);
-        assertEquals("100/0 => 100", 100.0f, Ratio.create(100,0).getPercentageFloat(), 0.005f);
+        // assertEquals("1/0   => 100", 100.0f, Ratio.create(  1,0).getPercentageFloat(), 0.005f);
+        // assertEquals("100/0 => 100", 100.0f, Ratio.create(100,0).getPercentageFloat(), 0.005f);
 
         assertEquals("0/5 =>   0",   0.0f, Ratio.create(0,5).getPercentageFloat(), 0.005f);
         assertEquals("4/5 =>  80",  80.0f, Ratio.create(4,5).getPercentageFloat(), 0.005f);
