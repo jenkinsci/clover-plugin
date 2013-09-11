23fcafd041a5ef76e25bbdfcc6733bbeb6116c99
Brian Wightman <MidLifeXis@wightmanfam.org>
2013-09-11 12:23:15 -0500

    Add test cases for expected behavior
    Add test cases for when the denominator is 0, and cases where denominator is less than numerator.

--- src/test/java/hudson/plugins/clover/RatioTest.java
+++ src/test/java/hudson/plugins/clover/RatioTest.java
@@ -21,4 +21,25 @@ public class RatioTest extends TestCase {
     public void testParseValue() throws Exception {
         assertRatio(Ratio.create(1,2), 1.0f, 2.0f);
     }
+    
+    /**
+     * Tests that {@link Ratio#getPercentageFloat()} handles appropriate
+     * values for numerator and denominator.  Specifically:
+     * 
+     * 1 - Ratios where the denominator is 0 (no tests), return 100%.
+     * 
+     * 2 - All other ratios return the numerator / denominiator.
+     *
+     * @throws Exception
+     */
+    public void testGetPercentageFloat() throws Exception {
+        assertEquals(Ratio.create(  0,0).getPercentageFloat(), 100.0f, 0.005f);
+        assertEquals(Ratio.create(  1,0).getPercentageFloat(), 100.0f, 0.005f);
+        assertEquals(Ratio.create(100,0).getPercentageFloat(), 100.0f, 0.005f);
+
+        assertEquals(Ratio.create(0,5).getPercentageFloat(),   0.0f, 0.005f);
+        assertEquals(Ratio.create(4,5).getPercentageFloat(),  80.0f, 0.005f);
+        assertEquals(Ratio.create(5,5).getPercentageFloat(), 100.0f, 0.005f);
+    }
+
 }
